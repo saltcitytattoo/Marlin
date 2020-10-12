c7db6f659f637b82cd7beb1201c116956d3a035f
Curtis  Evans <curtis@hammerton.com>
2020-10-12 10:24:46 -0600

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3.H1"
+#define CUSTOM_MACHINE_NAME "Ender-3.H1 10-12-20"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
