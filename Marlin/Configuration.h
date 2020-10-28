f8db89bbd0d11f2c4bd8a6d1cc93381c5d5beb18
saltcitytattoo <51271361+saltcitytattoo@users.noreply.github.com>
2020-10-28 11:59:10 -0600

    wiz
    wiz

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3.H1 10-22-20"
+#define CUSTOM_MACHINE_NAME "Ender-3.H1 10-28-20"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
